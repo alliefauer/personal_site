24ae93d92563d1b62dc48f8220961fb489858b36
Allie Fauer <alliefauer@gmail.com>
2017-10-20 11:02:54 -0400

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         uglify: {
-          files: ['Gruntfile.js', 'src/components/About.js', 'src/components/Image.js', 'src/components/Jumbotron.js', 'src/components/MenuBars.js', 'src/components/MobileNavbar.js', 'src/components/NavbarMain.js', 'src/components/Project.js', 'src/components/Projects.js', 'src/components/WebNavbar.js'],
+          files: {
+            'dest/output.min.js': ['Gruntfile.js', 'src/components/About.js', 'src/components/Image.js', 'src/components/Jumbotron.js', 'src/components/MenuBars.js', 'src/components/MobileNavbar.js', 'src/components/NavbarMain.js', 'src/components/Project.js', 'src/components/Projects.js', 'src/components/WebNavbar.js']
+          },
           options: {
             mangle: false,
             drop_console: true,
