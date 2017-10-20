d6f34196cce50acc85ddfa57730a215969c3e858
Allie Fauer <alliefauer@gmail.com>
2017-10-19 23:45:36 -0400

    more uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
         uglify: {
           files: ['Gruntfile.js', 'src/components/About.js', 'src/components/Image.js', 'src/components/Jumbotron.js', 'src/components/MenuBars.js', 'src/components/MobileNavbar.js', 'src/components/NavbarMain.js', 'src/components/Project.js', 'src/components/Projects.js', 'src/components/WebNavbar.js'],
           options: {
+            mangle: false,
+            drop_console: true,
+            beautify: true,
             globals: {
               jQuery: true
             }
